d9deccfab6b3a080bfd2925cc695a5d39c67853e
David Keyes <david@rfortherestofus.com>
2022-06-13 11:38:30 -0700

    Update TFFF basemap function

---
+++ R/geospatial.R
@@ -0,0 +1,19 @@
+#' Function to get an sf object with the TFFF service area (Oregon and Siskiyou
+#' County, California)
+#'
+#' @return sf object with Oregon counties and Siskiyou County, California
+#' @export
+#'
+tfff_get_basemap <- function() {
+
+  options(tigris_class = "sf")
+
+  tfff_basemap <- tigris::counties(cb = TRUE, class="sf") %>%
+    janitor::clean_names() %>%
+    dplyr::filter(statefp == "41" | statefp == "06") %>%
+    dplyr::filter(statefp == "41" | name == "Siskiyou") %>%
+    dplyr::select(name, geoid) %>%
+    dplyr::rename(geography = name)
+
+  tfff_basemap
+}
