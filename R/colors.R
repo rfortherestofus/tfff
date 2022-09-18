f1ca35ee5f5813a1a9731cbfbd805940ded9d35a
Thomas Vroylandt <tvroylandt@gmail.com>
2022-09-18 09:29:08 +0200

    Adding transparency function

--- R/colors.R
+++ R/colors.R
@@ -32,3 +32,33 @@ tfff_colors <- function(...) {
   tfff_colors_vector[cols] %>%
     as.vector()
 }
+
+#' Transparency color - from https://www.dataanalytics.org.uk/make-transparent-colors-in-r/
+#'
+#' @param color A color
+#' @param percent Transparency shade
+#'
+#' @return A color hex
+#' @export
+#'
+#' @example t_col(tfff_colors("Dark Green"),50)
+t_col <- function(color, percent = 50) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(
+    rgb.val[1],
+    rgb.val[2],
+    rgb.val[3],
+    max = 255,
+    alpha = (100 - percent) * 255 / 100
+  )
+
+  ## Save the color
+  return(t.col)
+}
